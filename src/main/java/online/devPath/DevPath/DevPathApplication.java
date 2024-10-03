56e17791e888e53fec5ef9892213e3a9a0473e2b
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-10-03 17:35:41 +0200

    conf: Load environment variables

--- src/main/java/online/devPath/DevPath/DevPathApplication.java
+++ src/main/java/online/devPath/DevPath/DevPathApplication.java
@@ -2,11 +2,19 @@ package online.devPath.DevPath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DevPathApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("DATASOURCE_DRIVER", dotenv.get("DATASOURCE_DRIVER"));
+		System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+		System.setProperty("DATASOURCE_USER", dotenv.get("DATASOURCE_USER"));
+		System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(DevPathApplication.class, args);
 	}
 
